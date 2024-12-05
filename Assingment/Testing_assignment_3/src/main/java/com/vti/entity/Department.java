7c550eca10b94ffa7e3d9055d42dd07362e396c7
phantrongvinh <1phantrongvinh98@gmail.com>
2024-12-05 19:14:27 +0700

    Department entity

--- Assingment/Testing_assignment_3/src/main/java/com/vti/entity/Department.java
+++ Assingment/Testing_assignment_3/src/main/java/com/vti/entity/Department.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -42,10 +41,6 @@ public class Department implements Serializable {
     public Department(short departmentID, String departmentName) {
         this.departmentID = departmentID;
         this.departmentName = departmentName;
-    }
-
-    public static long getSerialversionuid() {
-        return serialVersionUID;
     }
 
     public short getDepartmentID() {
